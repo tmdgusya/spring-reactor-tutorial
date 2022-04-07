5305c7a0c6fab605dc202c6e11caaddcbd59678e
tmdgusya <tmdgusya@naver.com>
2022-04-07 23:35:14 +0900

    init

---
+++ src/main/kotlin/com/roach/reactivespringstudy/ReactiveSpringStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.roach.reactivespringstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveSpringStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactiveSpringStudyApplication>(*args)
+}
